4385d3ca2ccd01f9abfee879cf1b3792c04b1512
Watermelon13371437 <emaildlyaunivera@gmail.com>
2025-12-08 02:43:38 +0300

    Update receipt.h
    working on a fix of receipt

--- src/receipt.h
+++ src/receipt.h
@@ -8,30 +8,40 @@
 using namespace std;
 
 class Receipt : public print {
-    private:
+private:
+    parcel& linkedParcel;   // reference
     double Paid;
     string ETA;
     string timestamp;
 
-    public:
-    Receipt(double price){
+public:
+    Receipt(parcel& p, double price)
+    : linkedParcel(p)
+    {
         Paid = price;
+        timestamp = GetTime();
     }
+
+    void setETA(string eta){
+        ETA = eta;
+    }
+
     void printReceipt(){
-        cout<< space << postOffice << endl;
-        //cout << SetWidth(36, "CLERK:", ClerkName)<<endl;
+
+        cout << space << postOffice << endl;
         cout << SetWidth(36, "POST OFFICE:", "EGHAM, Church Road 67")<<endl;
-        cout << GetTime();
-        cout  space << MiddleStr(36, "RECEIPT") << space;
-        
-        cout << SetWidth(36, "RECEIPT:", to_string(getID())) << endl;
-        //cout << SetWidth(36, "SENDER:", SenderName) << endl;
-        //cout << SetWidth(36, "RECEIVER:", getReceiverName()) << endl;
-        cout << SetWidth(36, "WEIGHT:", DoubleToStr(getWeight()) + " kg") << endl; 
+        cout << timestamp;
+
+        cout << space << MiddleStr(36, "RECEIPT") << space;
+
+        cout << SetWidth(36, "RECEIPT:", to_string(getID())) << endl; 
+        cout << SetWidth(36, "DEST:", linkedParcel.getDestination()) << endl;
+        cout << SetWidth(36, "WEIGHT:", DoubleToStr(linkedParcel.getWeight()) + " kg") << endl;
         cout << SetWidth(37, "TOTAL PAID £:", DoubleToStr(Paid)) << endl;
-        cout << SetWidth(36, "TRACKING:", getTrackingNumber());
+        cout << SetWidth(36, "TRACKING:", linkedParcel.getTrackingNumber()) << endl; 
         cout << SetWidth(36, "ETA:", ETA);
         cout <<space << "         ***THANK YOU!!!***         " << space;
     }
 };
+
 #endif
